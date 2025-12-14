8a63736b4e3fa4110ed2053966a87d1730a11e09
Shaun Laing <shaunpl2006@outlook.com>
2025-12-14 02:05:29 +0000

    Integrate DSharpPlus library and initial bot configuration with command, event, and service setup.

--- DiscordBot/Bot.cs
+++ DiscordBot/Bot.cs
@@ -1,5 +1,10 @@
 ﻿using DiscordBot.Exceptions;
+using DSharpPlus;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Commands.Processors.TextCommands;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace DiscordBot;
 
@@ -21,5 +26,50 @@ public class Bot
             .Value ?? throw new DiscordTokenNotFound();
     }
     
+    public async Task Run(CancellationToken ct)
+    {
+        var builder = DiscordClientBuilder.CreateDefault(Token,
+            SlashCommandProcessor.RequiredIntents | TextCommandProcessor.RequiredIntents);
+        
+        ConfigureServices(builder);
+        ConfigureEvents(builder);
+        ConfigureCommands(builder);
+
+        var client = builder.Build();
+        await client.ConnectAsync().ConfigureAwait(false);
+
+        ct.Register(async void () =>
+        {
+            await client.DisconnectAsync().ConfigureAwait(false);
+        });
+    }
+
+    private void ConfigureServices(DiscordClientBuilder builder)
+    {
+        builder.ConfigureLogging(loggingBuilder =>
+        {
+            loggingBuilder.SetMinimumLevel(LogLevel.Debug);
+        });
+        
+        builder.ConfigureServices(services =>
+        {
+
+        });
+    }
+
+    private void ConfigureEvents(DiscordClientBuilder builder)
+    {
+        builder.ConfigureEventHandlers(handlers =>
+        {
+
+        });
+    }
     
+    private void ConfigureCommands(DiscordClientBuilder builder)
+    {
+        builder.UseCommands((provider, extension) =>
+        {
+
+        });
+    }
 }
