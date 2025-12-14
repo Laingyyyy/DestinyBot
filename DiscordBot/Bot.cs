da160106cbf1066ebca6765eaf8b692837f9408e
Shaun Laing <shaunpl2006@outlook.com>
2025-12-14 01:31:35 +0000

    Add DiscordBot project with user secrets integration and custom exception for missing Discord token.

---
+++ DiscordBot/Bot.cs
@@ -0,0 +1,25 @@
+﻿using DiscordBot.Exceptions;
+using Microsoft.Extensions.Configuration;
+
+namespace DiscordBot;
+
+public class Bot
+{
+    // Discord Token Stored In Secrets Manager At "Discord:Token"
+    private readonly string Token;
+    
+    // 
+    
+    public Bot()
+    {
+        var config = new ConfigurationBuilder();
+        config.AddUserSecrets<Bot>();
+        
+        Token = config
+            .Build()
+            .GetSection("Discord:Token")
+            .Value ?? throw new DiscordTokenNotFound();
+    }
+    
+    
+}
---
+++ DiscordBot/Exceptions/DiscordTokenNotFound.cs
@@ -0,0 +1,21 @@
+﻿namespace DiscordBot.Exceptions;
+
+public class DiscordTokenNotFound : Exception
+{
+    public DiscordTokenNotFound()
+    {
+        
+    }
+
+    public DiscordTokenNotFound(string message)
+        : base(message)
+    {
+        
+    }
+    
+    public DiscordTokenNotFound(string message, Exception innerException)
+        : base(message, innerException)
+    {
+        
+    }
+}
